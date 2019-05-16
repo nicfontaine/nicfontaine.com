408c75d88e87ec153f2ec848089a78249343496c
Nic Fontaine <nic@nicfontaine.com>
2019-05-16 12:51:51 -0700

    + web screens and recordings

--- js/preload.js
+++ js/preload.js
@@ -4,6 +4,7 @@
 
 		var preloader = {
 			design: [
+				"./img/design/experience-download-page-c.png",
 				"./img/m-styleguide-01-c.png",
 				"./img/m-styleguide-02-c.png",
 				"./img/m-styleguide-03-c.png",
